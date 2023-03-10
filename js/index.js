eedc288c8dae197a07fc15d7cf4d3ccfa611c0d7
Pabla Bertani <pabla.89.bertani@gmail.com>
2023-03-10 10:40:13 -0300

    index, upcoming y past events con funciones de orden superior, ok

--- js/index.js
+++ js/index.js
@@ -4,24 +4,47 @@ let conteinerCard = document.getElementById("conteiner-card");
 let stringHtml = ``;
 
 function makingCards() {
-  for (let i = 0; i < allEvents.length; i++) {
-    stringHtml += `
+allEvents.forEach(events=>{
+  stringHtml += `
   <div class="card position-relative" style="width: 17rem;">
-    <img src=${allEvents[i].image} class="card-img-top img-card" alt=${allEvents[i].name} >
+    <img src=${events.image} class="card-img-top img-card" alt=${events.name} >
       <div class="card-body">
-        <h5 class="card-title">${allEvents[i].name}</h5>
-        <p class="card-text">${allEvents[i].date}</p>
-        <p class="card-text">${allEvents[i].description}</p>
+        <h5 class="card-title">${events.name}</h5>
+        <p class="card-text">${events.date}</p>
+        <p class="card-text">${events.description}</p>
       </div>
       <span class="row justify-content-around mb-3">
-        <p class="col-5 pt-2">$${allEvents[i].price}</p>
+        <p class="col-5 pt-2">$${events.price}</p>
         <a href="./details.html" class="col-5 btn-more">See More</a>
       </span>
  </div>
   `;
-  }
+});
 }
 
 makingCards();
 
 conteinerCard.innerHTML = stringHtml;
+
+// function makingCards() {
+//   for (let i = 0; i < allEvents.length; i++) {
+//     stringHtml += `
+//   <div class="card position-relative" style="width: 17rem;">
+//     <img src=${allEvents[i].image} class="card-img-top img-card" alt=${allEvents[i].name} >
+//       <div class="card-body">
+//         <h5 class="card-title">${allEvents[i].name}</h5>
+//         <p class="card-text">${allEvents[i].date}</p>
+//         <p class="card-text">${allEvents[i].description}</p>
+//       </div>
+//       <span class="row justify-content-around mb-3">
+//         <p class="col-5 pt-2">$${allEvents[i].price}</p>
+//         <a href="./details.html" class="col-5 btn-more">See More</a>
+//       </span>
+//  </div>
+//   `;
+//   }
+// }
+
+// makingCards();
+
+// conteinerCard.innerHTML = stringHtml;
--- js/upcoming_events.js
+++ js/upcoming_events.js
@@ -2,34 +2,64 @@ const currentDate = data.currentDate;
 
 const allEvents = data.events;
 
-const upcomingEvents = [];
+// const upcomingEvents = [];
 
 let conteinerCard = document.getElementById("conteiner-cardUpcomingEvents");
 
 let stringHtml = ``;
 
+
+const upcomingEvents = allEvents.filter(events => events.date > currentDate);
+//console.log(upcomingEvents)
+
 function makingCards() {
-  for (let i = 0; i < allEvents.length; i++) {
-    if (allEvents[i].date > currentDate) {
-      stringHtml += `
-   <div class="card position-relative" style="width: 17rem;">
-     <img src=${allEvents[i].image} class="card-img-top img-card" alt=${allEvents[i].name} >
-       <div class="card-body">
-         <h5 class="card-title">${allEvents[i].name}</h5>
-         <p class="card-text">${allEvents[i].date}</p>
-         <p class="card-text">${allEvents[i].description}</p>
-       </div>
-       <span class="row justify-content-around mb-3">
-         <p class="col-5 pt-2">$${allEvents[i].price}</p>
-         <a href="./details.html" class="col-5 btn-more">See More</a>
-       </span>
-  </div>
-   `;
-      upcomingEvents.push(allEvents[i]);
-    }
+  upcomingEvents.forEach(events=>{
+    stringHtml += `
+    <div class="card position-relative" style="width: 17rem;">
+      <img src=${events.image} class="card-img-top img-card" alt=${events.name} >
+        <div class="card-body">
+          <h5 class="card-title">${events.name}</h5>
+          <p class="card-text">${events.date}</p>
+          <p class="card-text">${events.description}</p>
+        </div>
+        <span class="row justify-content-around mb-3">
+          <p class="col-5 pt-2">$${events.price}</p>
+          <a href="./details.html" class="col-5 btn-more">See More</a>
+        </span>
+   </div>
+    `;
+  });
   }
-}
+  
+  makingCards();
+  
+  conteinerCard.innerHTML = stringHtml;
 
-makingCards();
 
-conteinerCard.innerHTML = stringHtml;
+
+
+// function makingCards() {
+//   for (let i = 0; i < allEvents.length; i++) {
+//     if (allEvents[i].date > currentDate) {
+//       stringHtml += `
+//    <div class="card position-relative" style="width: 17rem;">
+//      <img src=${allEvents[i].image} class="card-img-top img-card" alt=${allEvents[i].name} >
+//        <div class="card-body">
+//          <h5 class="card-title">${allEvents[i].name}</h5>
+//          <p class="card-text">${allEvents[i].date}</p>
+//          <p class="card-text">${allEvents[i].description}</p>
+//        </div>
+//        <span class="row justify-content-around mb-3">
+//          <p class="col-5 pt-2">$${allEvents[i].price}</p>
+//          <a href="./details.html" class="col-5 btn-more">See More</a>
+//        </span>
+//   </div>
+//    `;
+//       upcomingEvents.push(allEvents[i]);
+//     }
+//   }
+// }
+
+// makingCards();
+
+// conteinerCard.innerHTML = stringHtml;
