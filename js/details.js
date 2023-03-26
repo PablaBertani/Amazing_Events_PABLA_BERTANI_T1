507fa4aa6338d1a8d3fb2829f5708e8af68f028f
Pabla Bertani <pabla.89.bertani@gmail.com>
2023-03-25 23:52:09 -0300

    Proyecto Terminado,optimizado, con código validado ok.

--- js/details.js
+++ js/details.js
@@ -1,19 +1,20 @@
 fetch("https://mindhub-xj03.onrender.com/api/amazing")
   .then((response) => response.json())
   .then((events) => {
-    const allEvents = events.events; 
+    const allEvents = events.events;
 
     const params = new URLSearchParams(location.search);
-    const conteinerCard = document.getElementById("conteiner-cardDetails");
     
-let id = params.get("id");
-console.log(id);
+    const conteinerCard = document.getElementById("conteiner-cardDetails");
 
-let cardDetails = allEvents.find((events) => events._id == id);
+    let id = params.get("id");
+    console.log(id);
 
-let detailsHtml = ``;
+    let cardDetails = allEvents.find((events) => events._id == id);
 
-detailsHtml += `
+    let detailsHtml = ``;
+
+    detailsHtml += `
       <div class="row my-4">
          <div class="col-md-6 col-sm-4">
            <img
@@ -53,67 +54,4 @@ detailsHtml += `
         </div>
     `;
     conteinerCard.innerHTML = detailsHtml;
-
-   
   });
-
-
-
-
-
-
-
-
-//const allEvents = data.events;
-
-//const conteinerCard = document.getElementById("conteiner-cardDetails");
-
-// const params = new URLSearchParams(location.search);
-
-// let id = params.get("id");
-// console.log(id);
-
-// let cardDetails = allEvents.find((events) => events._id == id);
-
-// let detailsHtml = ``;
-
-// detailsHtml += `
-//       <div class="row my-4">
-//          <div class="col-md-6 col-sm-4">
-//            <img
-//              src="${cardDetails.image}"
-//              class="img-fluid ps-2"
-//              alt="${cardDetails.image}"
-//            >
-//          </div>
-//          <div class="col-md-6 col-sm-4">
-//            <div class="card-body details px-1">
-//              <h5 class="card-title">${cardDetails.name}</h5>
-//              <p class="card-text"><b>${cardDetails.date}</b></p>
-//              <p class="card-text text-start text-wrap  lh-sm">${
-//                cardDetails.description
-//              }</p>
-//              <p class="card-text text-start">Category: ${
-//                cardDetails.category
-//              }</p>
-//              <p class="card-text text-start">Capacity: ${
-//                cardDetails.capacity
-//              }</p>
-//              <p class="card-text text-start">${
-//                cardDetails.assistance !== undefined
-//                  ? "Assistence: " + cardDetails.assistance
-//                  : ""
-//              }</p>
-//              <p class="card-text text-start">${
-//                cardDetails.estimate !== undefined
-//                  ? "Estimate: " + cardDetails.estimate
-//                  : ""
-//              }</p>
-//              <p class="card-text text-start"><b>Price: $${
-//                cardDetails.price
-//              }</b></p>
-//            </div>
-//          </div>
-//         </div>
-//     `;
-// conteinerCard.innerHTML = detailsHtml;
